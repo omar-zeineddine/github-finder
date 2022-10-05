b59fba3d6b07907d70dc08f5a1b2778a1138cc8c
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-05 18:15:55 +0300

    refactor - dispatch in use effect

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -16,3 +16,36 @@ export const searchUsers = async (text) => {
 
   return items;
 };
+
+// get single user
+export const getUser = async (login) => {
+  const response = await fetch(`${URL}/users/${login}`, {
+    headers: {
+      Authorization: `token ${TOKEN}`,
+    },
+  });
+
+  if (response.status === 404) {
+    window.location = "/notfound";
+  } else {
+    const data = await response.json();
+    return data;
+  }
+};
+
+export const getUserRepos = async (login) => {
+  // search params
+  const params = new URLSearchParams({
+    sort: "created",
+    per_page: 5,
+  });
+
+  const response = await fetch(`${URL}/users/${login}/repos?${params}`, {
+    headers: {
+      Authorization: `token ${TOKEN}`,
+    },
+  });
+
+  const data = await response.json();
+  return data;
+};
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,16 +4,25 @@ import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext";
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import Spinner from "../components/layout/Spinner";
+import { getUser, getUserRepos } from "../context/github/GithubActions";
 
 const User = () => {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
+    dispatch({ type: "SET_LOADING" });
+
+    const getUserData = async () => {
+      const userData = await getUser(params.login);
+      dispatch({ type: "GET_USER", payload: userData });
+
+      const userRepoData = await getUserRepos(params.login);
+      dispatch({ type: "GET_REPOS", payload: userRepoData });
+    };
+
+    getUserData();
   }, []);
 
   const {
