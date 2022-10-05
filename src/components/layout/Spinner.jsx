60f2f322514b04a3dfbea92cc365f4f041c09c1c
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-05 20:52:12 +0300

    refactor - update svg spinner

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -1,11 +1,11 @@
-import spinner from "./assets/spinner.gif";
+import spinner from "./assets/spinner.svg";
 
 const Spinner = () => {
   return (
     <div className="w-100 mt-20">
       <img
         className="text-center mx-auto"
-        width={180}
+        width={75}
         src={spinner}
         alt="loading..."
       />
