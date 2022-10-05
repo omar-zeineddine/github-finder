812600811bf4117d10c7b235ac7b000df9b268f9
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-05 20:52:26 +0300

    refactor - update about page styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-6xl mb-4">Github Finder</h1>
-      <p className="mb-4 text-2xl font-light">
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="text-6xl mb-4 text-center">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light text-center px-4">
         A React app to search GitHub profiles and see profile details.
       </p>
       <p className="text-lg text-gray-400">
