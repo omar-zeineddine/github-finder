2177ce3ab6b8134284cc54de60d28848dc19cf5d
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-05 11:30:19 +0300

    feat - create user item component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -30,7 +30,7 @@ const UserResults = () => {
       <div>
         <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
           {users.map((user) => {
-            return <h2>{user.login}</h2>;
+            return <UserItem key={user.id} user={user} />;
           })}
         </div>
       </div>
