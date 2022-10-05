608e4d9f77e9d5c9422dc74ea82b2a221dfd7296
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-05 19:39:01 +0300

    refactor - adjust search input position

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -7,9 +7,8 @@ const Navbar = ({ title }) => {
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <FaGithub className="inline pr-2 text-5xl" />
           <Link to="/" className="text-lg font-bold align-middle">
-            {title}
+            <FaGithub className="inline pr-2 text-5xl" /> {title}
           </Link>
         </div>
         <div className="flex-1 px-2 mx-2">
